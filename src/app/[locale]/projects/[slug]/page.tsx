d3745078d51dfa0af1ecc64673c35ddbeb9272bb
Alysson Rafael <alyssonrafael11@gmail.com>
2025-05-26 22:00:51 -0300

    correção do params para build no arquivo page teve ser async por padrao

--- src/app/[locale]/projects/[slug]/page.tsx
+++ src/app/[locale]/projects/[slug]/page.tsx
@@ -1,25 +1,22 @@
 import { notFound } from "next/navigation";
 import dynamic from "next/dynamic";
 
-// inporta os componentes dinamicamente para mais performace
 const projetoComponents: Record<string, React.ComponentType> = {
-  "Projeto-1": dynamic(
-    () => import("../../../../components/projetos/Projeto-1")
-  )
+  "Projeto-1": dynamic(() => import("../../../../components/projetos/Projeto-1")),
 };
 
-export default function ProjetoPage({
+export default async function ProjetoPage({
   params,
 }: {
-  params: { slug: string; locale: string };
+  params: Promise<{ slug: string; locale: string }>;
 }) {
-  //passa o componente dinamicamente baseado no slug
-  const ProjetoComponent = projetoComponents[params.slug];
-  //se nao tiver o componente volta 404
+  const resolvedParams = await params;
+  const ProjetoComponent = projetoComponents[resolvedParams.slug];
+  
   if (!ProjetoComponent) return notFound();
-  //se tiver o componente renderiza ele
+
   return (
-   <main className="max-w-7xl mx-auto px-4">
+    <main className="max-w-7xl mx-auto px-4">
       <ProjetoComponent />
     </main>
   );
