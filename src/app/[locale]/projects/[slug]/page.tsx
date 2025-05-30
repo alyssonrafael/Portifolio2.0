a04ad855d91e4fdb3373904a01f8af7491d40099
Alysson Rafael <alyssonrafael11@gmail.com>
2025-05-29 22:36:30 -0300

    fix: erro do parms async no momento do build

--- src/app/[locale]/projects/[slug]/page.tsx
+++ src/app/[locale]/projects/[slug]/page.tsx
@@ -3,9 +3,10 @@ import ProjetoPageContent from "@/components/projetos/ProjetoPageContent";
 import { projects } from "@/data/projects";
 
 //pega o slug por parametro
-export default function ProjetoPage({ params }: { params: { slug: string } }) {
+export default async function ProjetoPage({ params }: { params: Promise<{ slug: string }> }) {
+  const resolvedParams = await params;
   //busca o slug corespondente no data do projects
-  const projeto = projects.find((p) => p.slug === params.slug);
+  const projeto = projects.find((p) => p.slug === resolvedParams.slug);
   // se nao achar 404
   if (!projeto) return notFound();
   //se achar retorna a pagina padrao dos projetos com todas as props do projeto encontrado no data
